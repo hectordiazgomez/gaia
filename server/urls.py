867421cbdbb13f1055be361f6d2e846468f286aa
Hector Alberto Diaz Gomez <146489883+hectordiazgomez@users.noreply.github.com>
2024-08-13 00:33:15 -0500

    Update urls.py

--- server/urls.py
+++ server/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('train-nmt/', views.train_nmt, name='train_nmt'),
+    path('translate/', views.translation_endpoint, name='translation_endpoint'),
 ]
